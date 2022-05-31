b003861e09c2abccc941b0b7334619f46332c326
Nabokov79 <nabokovsg@yandex.ru>
2022-05-31 22:28:36 +0300

    add exception handler

--- src/main/java/ru/yandex/practicum/filmorate/exeption/ControllerException.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate.exeption;
-
-public class ControllerException extends RuntimeException {
-
-    public ControllerException(String message) {
-        super(message);
-    }
-}
