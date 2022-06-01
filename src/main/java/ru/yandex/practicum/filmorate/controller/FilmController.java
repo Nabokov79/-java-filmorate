58c634e84131a57f687d4e3c9f897a2be15e43fd
Nabokov79 <nabokovsg@yandex.ru>
2022-06-01 21:01:03 +0300

    add controller

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,8 +1,8 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import ru.yandex.practicum.filmorate.exeption.CustomException;
 import ru.yandex.practicum.filmorate.model.Film;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
@@ -18,18 +18,32 @@ public class FilmController {
 
     private final Map<Long,Film> films = new HashMap<>();
 
+    public long createId() {
+        long id = 0;
+        for(Long i : films.keySet()) {
+            if (id < i) {
+                id = i;
+            }
+        }
+        return id;
+    }
+
     @PostMapping
     public ResponseEntity<Film> createFilm(@Valid @RequestBody Film film) {
+        film.setId(createId() + 1);
         if (films.containsKey(film.getId())) {
             log.info("Ошибка добавления данных: " + film.getName());
             return ResponseEntity.badRequest().body(film);
         }
-        films.put(film.addNewIdFilm(), film);
+        films.put(film.getId(), film);
         return ResponseEntity.ok().body(film);
     }
 
    @PutMapping
    public ResponseEntity<Film> updateFilm(@Valid @RequestBody Film film) {
+        if (film.getId() < 0) {
+            throw new CustomException("Ошибка сервера.");
+        }
         if (!films.containsKey(film.getId())) {
             log.info("Ошибка обновления данных Film: " + film.getName());
             return ResponseEntity.badRequest().body(film);
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,11 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import ru.yandex.practicum.filmorate.exeption.CustomException;
 import ru.yandex.practicum.filmorate.model.User;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,18 +17,32 @@ public class UserController {
 
     private final Map<Long, User> users = new HashMap<>();
 
+    public long createId() {
+        long id = 0;
+        for(Long i : users.keySet()) {
+            if (id < i) {
+                id = i;
+            }
+        }
+        return id + 1;
+    }
+
     @PostMapping
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        user.setId(createId());
         if (users.containsKey(user.getId())) {
             log.info("Ошибка добавления данных пользователя: " + user.getName());
             return ResponseEntity.badRequest().body(user);
         }
-        users.put(user.addNewIdUser(), user);
+        users.put(user.getId(), user);
         return ResponseEntity.ok().body(user);
     }
 
     @PutMapping
     public ResponseEntity<User> updateUser(@Valid @RequestBody User user) {
+        if (user.getId() < 0) {
+            throw new CustomException("Ошибка сервера.");
+        }
         if (!users.containsKey(user.getId())) {
             log.info("Ошибка обновления данных пользователя: " + user.getName());
             return ResponseEntity.badRequest().body(user);
