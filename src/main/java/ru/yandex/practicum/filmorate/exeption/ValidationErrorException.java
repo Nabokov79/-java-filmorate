5e5a92e8ff693e344b274fe06dfadea71ab0ec82
Nabokov79 <nabokovsg@yandex.ru>
2022-06-01 21:00:41 +0300

    add exception handler

--- src/main/java/ru/yandex/practicum/filmorate/exeption/ValidationErrorException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/ValidationErrorException.java
@@ -1,16 +1,11 @@
 package ru.yandex.practicum.filmorate.exeption;
 
-import org.apache.coyote.Response;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 
 import javax.validation.ConstraintViolationException;
 import java.util.List;
